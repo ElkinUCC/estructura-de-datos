a5685161337cf8ae3382d97689e0fd6b47263917
ElkinUCC <elkin.amadorp@campusucc.edu.co>
2026-02-07 16:35:03 +0000

    primer ejercicio en clase

---
+++ clase1.py
@@ -0,0 +1,6 @@
+edad = int(input ("Por favor, ingrese la edad:"))
+print("La evaluación de la expresión es:", edad <=18)
+if edad <= 18: 
+    print ("Es permitido")
+else:
+    print("No es permitido")
